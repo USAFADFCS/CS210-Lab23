23d4e308cfc30f1fbd54f7029036bb13abee5c9e
Ben McGraw <benjamin.mcgraw@afacademy.af.edu>
2022-10-14 02:44:56 +0000

    test commit

--- lab23functs.h
+++ lab23functs.h
@@ -11,5 +11,23 @@
 #define LAB23FUNCTS_H
 
 // YOUR CODE GOES HERE
+typedef struct Pokemon_struct
+{
+    char name[];
+    int hp;
+    int attack;
+    int defense;
+    double cp;
+}Pokemon;
+
+int readPokemonData(char filename[], Pokemon array[]);
+
+void printPokemon(Pokemon pokemon);
+
+void calculateCombatPower(Pokemon array[], int size);
+
+Pokemon* getPokemon(char* name, Pokemon array[], int size);
+
+int predictWinner(Pokemon pokemon1, Pokemon pokemon2);
 
 #endif
