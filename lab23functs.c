c890cbb2bfec9b7bdd636ca2423d02076e62d150
kennethsample <kennethsample@hotmail.com>
2022-10-18 10:12:15 -0600

    Update lab23functs.c

--- lab23functs.c
+++ lab23functs.c
@@ -11,36 +11,3 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lab23functs.h"
-
-
-int readPokemonData(char filename[], Pokemon array[]){
-    int numPokemon = 0;
-    FILE* myFile  = NULL;
-    myFile = fopen(filename, "r");
-    while (!feof(myFile)) {
-        //pokemon_name hp attack defense speed
-
-    }
-    fclose(myFile);
-    return numPokemon;
-}
-
-void printPokemon(Pokemon pokemon) {
-
-}
-
-void calculateCombatPower(Pokemon array[], int size) {
-
-}
-
-Pokemon* getPokemon(char* name, Pokemon array[], int size) {
-    Pokemon* ptr = NULL;
-
-    return ptr;
-}
-
-int predictWinner(Pokemon pokemon1, Pokemon pokemon2) {
-    int winner = 0;
-
-    return winner;
-}
